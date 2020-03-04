119535feeec120f4885221774bac903ebd912ab4
vadimsusername <vadimburyakovsky@yahoo.com>
2020-03-04 02:35:02 -0500

    choose which employee role to add

--- app.js
+++ app.js
@@ -1,35 +1,182 @@
 const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+/*const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+​*/
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
+//const promptJSON = "[]"
+const employeePrompt = [
+    {
+        type: "input",
+        message: "Enter name: ",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Enter ID number: ",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Enter email: ",
+        name: "email"
+    }
+];
+const employeePromptJSON = JSON.stringify(employeePrompt);
+
+var managerPrompt = JSON.parse(employeePromptJSON);
+managerPrompt.push({type:"input",message:"Enter office number: ",name:"office"});
+//console.log(managerPrompt);
+//console.log(employeePrompt);
+var engineerPrompt = JSON.parse(employeePromptJSON);
+engineerPrompt.push({type:"input",message:"Enter GitHub username: ",name:"github"});
+//console.log(engineerPrompt);
+var internPrompt = JSON.parse(employeePromptJSON);
+internPrompt.push({type:"input",message:"Enter interns university: ",name:"school"});
+//console.log(internPrompt);
+/*
+const managerPrompt = [
+    {
+        type: "input",
+        message: "Enter name: ",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Enter ID number: ",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Enter email: ",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Enter office number: ",
+        name: "office"
+    }
+];
+
+
+const engineerPrompt = [
+    {
+        type: "input",
+        message: "Enter name: ",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Enter ID number: ",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Enter email: ",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Enter GitHub username: ",
+        name: "github"
+    }
+];
+
+const internPrompt = [
+    {
+        type: "input",
+        message: "Enter name: ",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Enter ID number: ",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Enter email: ",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Enter university: ",
+        name: "school"
+    }
+];
+*/
+const rolePrompt = [
+    {
+        type: "list",
+        message: "Choose the type of employee: ",
+        choices: ["Manager","Engineer","Intern"],
+        name: "role"
+
+    }
+];
+const addEmpPrompt = [
+    {
+        type: "confirm",
+        message: "Do you want to add another employee?",
+        name:"addEmployee"
+    }
+]
+
+var employees = [];
+
+    inquirer.prompt(rolePrompt).then(answers => {
+        var role = answers.role;
+        console.log(role);
+        
+        switch(role){
+            case "Manager":
+                return inquirer.prompt(managerPrompt);
+                break;
+            case "Engineer":
+                return inquirer.prompt(engineerPrompt);
+                break;
+            case "Intern":
+                return inquirer.prompt(internPrompt);
+                break;
+            default:
+                console.log("Switch default case");
+        }
+    }).then(answers => {
+        console.log("In second prompt answers");
+        /*
+        console.log(answers);
+        var properties = Object.keys(answers);
+        console.log(properties);
+        
+        console.log(ans4);
+        */
+       var empObj;
+       var lastProperty = Object.keys(answers)[3];
+       
+        switch(lastProperty){
+            case "office":
+                console.log("Manager role answers");
+                break;
+            case "github":
+                console.log("Engineer role answers");
+               break;
+            case "school":
+                console.log("Intern role answers");
+                break;
+            default:
+                console.log("Second switch default case");
+        }
+      
+    });
+  
+/*
+    empObj = new Manager(answers.name,answers.id,answers.email,answers.office);
+    console.log(empObj);
+    employees.push(empObj);
+    */
