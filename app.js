fdf9400665e2537327c57ff069811d26f508b96d
vadimsusername <vadimburyakovsky@yahoo.com>
2020-03-04 22:49:09 -0500

    creates output file

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const render = require("./lib/htmlRenderer");
 const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const employeePrompt = [
     {
@@ -112,5 +116,14 @@ var addAnother = true;
   console.log(employees);
   var roster = render(employees);
   console.log(roster)
+  if (!fs.existsSync(OUTPUT_DIR)){
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFile(outputPath,roster,function(err){
+      if(err){
+          throw err;
+      }
+      console.log('Saved!');    
+  })
 })();
 
